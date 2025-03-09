7f264c4167239cbfcfd7f322760d3f5033c35d95
Ayush Patil <Ayush Patil>
2025-03-09 23:27:56 +0530

    added transaction_dialog,pre-commit_hooks

---
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -0,0 +1,145 @@
+'use client';
+
+import { TransactionType } from '~/lib/types';
+import { ReactNode, useCallback } from 'react';
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '~/components/ui/dialog';
+import { DialogTrigger } from '@radix-ui/react-dialog';
+import { cn } from '~/lib/utils';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { CreateTransactionSchema, CreateTransactionSchemaType } from '~/schema/transaction';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '~/components/ui/form';
+import { Input } from '~/components/ui/input';
+import CategoryPicker from './CategoryPicker';
+import { Popover, PopoverContent, PopoverTrigger } from '~/components/ui/popover';
+import { Button } from '~/components/ui/button';
+import { format } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
+import { Calendar } from '~/components/ui/calendar';
+
+interface Props {
+    trigger: ReactNode;
+    type: TransactionType;
+}
+
+function CreateTransactionDialog({ trigger, type }: Props) {
+    const form = useForm<CreateTransactionSchemaType>({
+        resolver: zodResolver(CreateTransactionSchema),
+        defaultValues: {
+            type,
+            date: new Date(),
+        },
+    });
+
+    const handleCategoryChange = useCallback(
+        (category: string) => {
+            console.log(category);
+            form.setValue('category', category);
+        },
+        [form]
+    );
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>{trigger}</DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Create a new{' '}
+                        <span className={cn('m-1', type === 'income' ? 'text-emerald-500' : 'text-red-500')}>
+                            {type}
+                        </span>
+                        transaction
+                    </DialogTitle>
+                </DialogHeader>
+                <Form {...form}>
+                    <form className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="description"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Description</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                    <FormDescription>Transaction description (optional)</FormDescription>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="amount"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Amount</FormLabel>
+                                    <FormControl>
+                                        <Input type="number" {...field} />
+                                    </FormControl>
+                                    <FormDescription>Transaction description (required)</FormDescription>
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex items-center justify-between gap-2">
+                            <FormField
+                                control={form.control}
+                                name="category"
+                                render={() => (
+                                    <FormItem>
+                                        <FormLabel>Category</FormLabel>
+                                        <FormControl>
+                                            <CategoryPicker type={type} onChange={handleCategoryChange} />
+                                        </FormControl>
+                                        <FormDescription>Select a category for this transaction</FormDescription>
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="date"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Transaction Date</FormLabel>
+                                        <FormControl>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <FormControl>
+                                                        <Button
+                                                            variant="outline"
+                                                            className={cn(
+                                                                'w-[200px] pl-3 text-left font-normal',
+                                                                !field.value && 'text-muted-foreground'
+                                                            )}
+                                                        >
+                                                            {field.value ? (
+                                                                format(field.value, 'PPP')
+                                                            ) : (
+                                                                <span>Pick a date</span>
+                                                            )}
+                                                            <CalendarIcon className="ml-auto h-4 w-4" />
+                                                        </Button>
+                                                    </FormControl>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-auto p-0">
+                                                    <Calendar
+                                                        mode="single"
+                                                        selected={field.value}
+                                                        onSelect={field.onChange}
+                                                        initialFocus
+                                                    />
+                                                </PopoverContent>
+                                            </Popover>
+                                        </FormControl>
+                                        <FormDescription>Select a date for this transaction</FormDescription>
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default CreateTransactionDialog;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import React from 'react';
+import { Button } from '~/components/ui/button';
+import prisma from '~/lib/prisma';
+import CreateTransactionDialog from './_components/CreateTransactionDialog';
 
-function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+async function DashboardPage() {
+    const user = await currentUser();
+    if (!user) redirect('/sign-in');
+
+    const userSettings = await prisma.userSettings.findUnique({
+        where: {
+            userId: user.id,
+        },
+    });
+
+    if (!userSettings) redirect('/wizard');
+
+    return (
+        <div className="h-full bg-background">
+            <div className="border-b bg-card">
+                <div className="container flex flex-wrap items-center justify-center gap-6 py-8">
+                    <p className="text-3xl font-bold">Hello, {user.firstName}! 👋</p>
+                    <div className="flex items-center gap-3">
+                        <CreateTransactionDialog
+                            trigger={
+                                <Button
+                                    variant={'outline'}
+                                    className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
+                                >
+                                    New Income 🤑
+                                </Button>
+                            }
+                            type="income"
+                        />
+                        <CreateTransactionDialog
+                            trigger={
+                                <Button
+                                    variant={'outline'}
+                                    className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
+                                >
+                                    New expense 😤
+                                </Button>
+                            }
+                            type="expense"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Dashboard
+export default DashboardPage;
